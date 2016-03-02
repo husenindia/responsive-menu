2076382215dcb7a86eab6432288aac82fa3e83f4
husenindia <husentelwala@gmail.com>
2016-03-02 13:32:11 +0530

    First upload
    Testing Purpose in iPhone 6

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+﻿
+$(document).ready(function () {
+    $menuIcon = $('.menu-icon');    
+    menuAnimationTime = 500;
+    $menuIcon.click(function () {
+        $this = $(this);        
+        if ($this.hasClass('active')) {
+            $('.menubg-wrapper').fadeOut();
+            $('ul li').hide();
+            $('.menubg').css({
+                'transform': 'translate3d(0px, 0, 0px) scale(0, 0)'
+            });
+            setTimeout(function () {
+                $('.menubg').remove();
+                $('.menubg-wrapper').remove();                
+            }, menuAnimationTime);
+        }
+        else {
+            $('body').append('<div class="menubg-wrapper"></div>');
+
+            $('<div class="menubg"></div>').insertAfter($menuIcon).animate({
+            }, menuAnimationTime, function () {
+                $(this).css({ 'transform': 'translate3d(0px, 0, 0px) scale(110, 90)' });
+            });
+            setTimeout(function () {
+                $('.menubg-wrapper').fadeIn();                
+                $('ul').find('li').each(function (index) {
+                    $this = $(this);
+                    $this.delay(200 * index).fadeIn(500);
+                });
+            }, menuAnimationTime);
+        }
+        $this.toggleClass('active');
+        $('body').toggleClass('overflow-hide');
+        
+    })
+})
